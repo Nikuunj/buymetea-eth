f9e7ec78c730afcb01417d6842769b46d860a74f
Nikunj <mnikunj262@gmail.com>
2025-09-25 16:10:19 +0530

    update: prisma schema

---
+++ buymetea/prisma/migrations/20250925103930_username/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userName` on the `UserProfile` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userName]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "public"."UserProfile_userName_key";
+
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "userName" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."UserProfile" DROP COLUMN "userName";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "public"."User"("userName");
