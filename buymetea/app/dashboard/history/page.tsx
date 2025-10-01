d86f5353b70fc4b3ceec7e1e5d807d350a807b8f
Nikunj <mnikunj262@gmail.com>
2025-10-02 01:19:23 +0530

    update: history and msg page

--- buymetea/app/dashboard/history/page.tsx
+++ buymetea/app/dashboard/history/page.tsx
@@ -1,8 +1,9 @@
+import DepositList from "@/components/dashboard/DepositList"
 
 function HistoryPage() {
    return (
-      <div className=" w-full min-h-screen col-span-9">
-         HistoryPage
+      <div className=" w-full min-h-screen col-span-9 flex justify-center items-center  px-2 sm:px-10">
+         <DepositList to={'111'} />
       </div>
    )
 }
--- buymetea/app/dashboard/message/page.tsx
+++ buymetea/app/dashboard/message/page.tsx
@@ -1,8 +1,9 @@
+import MsgList from "@/components/dashboard/MsgList"
 
 function MessagePage() {
    return (
-      <div className=" w-full min-h-screen col-span-9">
-         MessagePage
+      <div className="w-full min-h-screen col-span-9 flex flex-col justify-center items-center px-2 sm:px-10">
+         <MsgList to={'111'} />
       </div>
    )
 }
