a358693e999ad84711dfffaf059c1b0d3d96d314
Nikunj <mnikunj262@gmail.com>
2025-10-02 01:18:32 +0530

    update: home main page and list box

--- buymetea/app/dashboard/page.tsx
+++ buymetea/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
-import MainDashbaord from "@/components/dashboard/MainDashbaord"
+import Profile from "@/components/User/Profile"
 
 function UserDashboard() {
    return (
-      <MainDashbaord />
+      <div className=" w-full min-h-screen col-span-9">
+         <Profile username="dsds" />
+      </div>
    )
 }
 
---
+++ buymetea/components/dashboard/ListBox.tsx
@@ -0,0 +1,14 @@
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { ReactNode } from "react"
+
+function ListBox({ children, to }: { children: ReactNode, to: string }) {
+   return (
+      <Link href={`./${to}`} className="w-full outline-0">
+         <div className="cursor-pointer w-full bg-zinc-100 p-4 space-y-1.5 rounded-md border border-zinc-200 ">{children}</div>
+      </Link>
+   )
+}
+
+export default ListBox
