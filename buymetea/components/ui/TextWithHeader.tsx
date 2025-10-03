5d180a55f8fec5872bd7c05a0e1cc1c0b4b465f2
Nikunj <mnikunj262@gmail.com>
2025-10-03 11:54:34 +0530

    update: user dash shown components

---
+++ buymetea/components/ui/TextWithHeader.tsx
@@ -0,0 +1,15 @@
+
+function TextWithHeader({ header, text }: { header: string, text: string }) {
+   return (
+      <div className="border-b space-y-2 p-4 border-zinc-400/85">
+         <p className="font-medium capitalize">
+            {header}
+         </p>
+         <p className="break-words text-zinc-700 whitespace-break-spaces">
+            {text}
+         </p>
+      </div>
+   )
+}
+
+export default TextWithHeader
