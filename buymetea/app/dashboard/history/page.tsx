a95f4b81af53988f35696003c9e2671efeddff05
Nikunj <mnikunj262@gmail.com>
2025-10-04 00:42:05 +0530

    update: fetch for msg

--- buymetea/app/dashboard/history/page.tsx
+++ buymetea/app/dashboard/history/page.tsx
@@ -1,10 +1,32 @@
+"use client"
 import DepositList from "@/components/dashboard/DepositList"
+import { trpc } from "@/utils/trpc";
 
 function HistoryPage() {
+
+   const { data, isLoading, isError, error } = trpc.deposit.get_depositList.useQuery(undefined,
+      {
+         refetchOnWindowFocus: false,
+         refetchOnReconnect: false,
+      }
+   );
+
+   if (isLoading) {
+      return <div>Loading...</div>
+   }
+
+   if (isError) {
+      return <div>Error: {error.message}</div>
+   }
+
+   if (!data?.deposit_list.length) {
+      return <div>No Deposit found.</div>
+   }
+   const renderDeposit = data.deposit_list.map(deposit => <DepositList id={deposit.transactionId} />)
    return (
-      <div className=" w-full min-h-screen col-span-9 flex justify-center items-center  px-2 sm:px-10">
-         <DepositList to={'111'} />
-      </div>
+      <>
+         {renderDeposit}
+      </>
    )
 }
 
