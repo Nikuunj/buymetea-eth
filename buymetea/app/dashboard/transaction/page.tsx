325ad8f9c1215bdedf57fd11616e4c900ac960b2
Nikunj <mnikunj262@gmail.com>
2025-10-04 00:41:36 +0530

    update: fetch and server to send token name

--- buymetea/app/dashboard/transaction/page.tsx
+++ buymetea/app/dashboard/transaction/page.tsx
@@ -1,10 +1,32 @@
+"use client"
 import TxList from "@/components/dashboard/TxList"
+import { trpc } from "@/utils/trpc";
 
 function TransactionPage() {
+   const { data, isLoading, isError, error } = trpc.tx.get_txList.useQuery(undefined,
+      {
+         refetchOnWindowFocus: false,
+         refetchOnReconnect: false,
+      }
+   );
+
+   if (isLoading) {
+      return <div>Loading...</div>
+   }
+
+   if (isError) {
+      return <div>Error: {error.message}</div>
+   }
+
+   if (!data?.tx_list.length) {
+      return <div>No Transaction found.</div>
+   }
+
+   const renderTxs = data.tx_list.map(tx => <TxList id={tx.id} amount={tx.amount} from={tx.from} tokenName={tx.tokenName}/>)
    return (
-      <div className="w-full min-h-screen col-span-9 flex justify-center items-center  px-2 sm:px-10">
-         <TxList to={'111'} />
-      </div>
+      <>
+         {renderTxs}
+      </>
    )
 }
 
