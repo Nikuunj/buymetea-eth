86d93dcb258cefc89e2127c4bc2e29610fe9a58b
Nikunj <mnikunj262@gmail.com>
2025-10-09 00:22:19 +0530

    fix: dashboard mobile view

--- buymetea/components/dashboard/LeftSide.tsx
+++ buymetea/components/dashboard/LeftSide.tsx
@@ -3,11 +3,14 @@
 import { links } from "@/utils/data/dashboardlink";
 import { useRouter } from "next/navigation"
 import DashboardLink from "../ui/DashboardLink";
-import { PowerOff } from "lucide-react";
+import { ListMinus, PowerOff } from "lucide-react";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import Button from "../ui/Button";
 
 function LeftSide() {
    const router = useRouter();
+   const [open, setOpen] = useState(false);
    function logout() {
       router.push('.')
       localStorage.removeItem('token');
@@ -17,21 +20,26 @@ function LeftSide() {
    
    const renderLink = links.map((li, i) => <DashboardLink key={i + li.title} text={li.title} to={li.to} />)
    return (
-      <div className=" w-full max-h-screen col-span-2 px-12 py-14 border-r border-zinc-300/80 flex flex-col justify-between">
-         <div className="flex flex-col gap-2">
-            {renderLink}
-         </div>
-
-         <motion.button 
-         whileTap={{
-            scale: 0.6
-         }}
-         transition={{ duration: 0.15 }}
-         onClick={logout}
-         className="hover:bg-red-100/90 p-2 w-fit rounded-full transition-all duration-300">
-            <PowerOff  className="text-red-800"/>
-         </motion.button>
+      <>
+      <div className="fixed md:hidden right-16 sm:right-24 top-3.5 sm:top-4 z-[50]">
+         <Button size="none" varient="outline" className="p-1  rounded-md" handleClick={() => setOpen(pre => !pre)}><ListMinus className="h-5 w-5"/></Button>
       </div>
+         <div className={` absolute md:relative ${ open ? 'block bg-white z-[60]' : 'hidden' } w-full max-h-screen col-span-0 md:col-span-2 px-4 lg:px-12 py-14 border-r items-start border-zinc-300/80 md:flex flex-col justify-between`}>
+            <div className="flex flex-col gap-2">
+               {renderLink}
+            </div>
+
+            <motion.button 
+            whileTap={{
+               scale: 0.6
+            }}
+            transition={{ duration: 0.15 }}
+            onClick={logout}
+            className="hover:bg-red-100/90 p-2 w-fit rounded-full transition-all duration-300">
+               <PowerOff  className="text-red-800"/>
+            </motion.button>
+         </div>
+      </>
    )
 }
 
--- buymetea/components/dashboard/MainDashbaord.tsx
+++ buymetea/components/dashboard/MainDashbaord.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 function MainDashbaord({ children }: { children: ReactNode }) {
    return (
-      <div className="w-full min-h-[90.5vh] overflow-hidden col-span-9 flex flex-col justify-center items-center">
+      <div className="w-full min-h-[90.5vh] overflow-hidden col-span-11 md:col-span-9 flex flex-col justify-center items-center">
          {children}
       </div>
    )
